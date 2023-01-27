94fb47ec45a4acb7053b14df2843e34590f0ae58
Bi Bamba <maxime-tizie@hotmail.fr>
2023-01-27 11:26:15 -0600

    create public subnets

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,27 +1,27 @@
 resource "aws_vpc" "main" {
-  cidr_block = var.vpc-cidr-block
+  cidr_block = var.vpc_cidr_block
 }
 
 resource "aws_network_acl" "netw-acl" {
   vpc_id = aws_vpc.main.id
 }
 
-resource "aws_subnet" "web-subnet01" {
+resource "aws_subnet" "private-subnet01" {
   vpc_id = aws_vpc.main.id
-  cidr_block = var.web-subnet-cidr-block
+  cidr_block = var.private_subnet01_cidr_block
 }
-resource "aws_subnet" "app-subnet02" {
+resource "aws_subnet" "private-subnet02" {
   vpc_id = aws_vpc.main.id
-  cidr_block = var.app-subnet-cidr-block
-}
-
-resource "aws_subnet" "app-subnet01" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = var.app-subnet-cidr-block
+  cidr_block = var.private-subnet02_cidr_block
 }
 
-resource "aws_subnet" "app-subnet02" {
+resource "aws_subnet" "public-subnet01" {
   vpc_id = aws_vpc.main.id
-  cidr_block = var.app-subnet-cidr-block
+  cidr_block = var.public_subnet_cidr_block
+}
+
+resource "aws_subnet" "public-subnet02" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.public_subnet02_cidr_block
 }
 
